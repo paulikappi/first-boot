86f7bf65c948389a46aaf362cb594f4cc4b1bae0
paulikappi <pauli.kappi@gmail.com>
2020-06-28 20:09:12 +0300

    removed visudo

--- first-boot.sh
+++ first-boot.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
-# run with sudo
+#run with sudo
 #wget -O - https://raw.githubusercontent.com/paulikappi/first-boot/master/first-boot.sh | sudo bash
-
-if [ -z "$1" ]; then
-
-  # When you run the script, you will run this block since $1 is empty.
-
-  echo "Starting up visudo with this script as first parameter"
-
-  # We first set this script as the EDITOR and then starts visudo.
-  # Visudo will now start and use THIS SCRIPT as its editor
-  export EDITOR=$0 && sudo -E visudo
-else
-
-  # When visudo starts this script, it will provide the name of the sudoers
-  # file as the first parameter and $1 will be non-empty. Because of that,
-  # visudo will run this block.
-
-  echo "Changing sudoers"
-
-  # We change the sudoers file and then exit
-  echo "Defaults    timestamp_timeout=0" >> $1
-fi
 
 apt update
 apt upgrade -y
