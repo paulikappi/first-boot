6b3efbe4a825f2b2dedf752af05cfc3e156831dd
paulikappi <pauli.kappi@gmail.com>
2020-06-28 19:50:35 +0300

    error fix 003

--- first-boot.sh
+++ first-boot.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # run with sudo
-#wget -O - https://raw.githubusercontent.com/paulikappi/first-boot/master/first-boot.sh | bash
-sudo echo "Editing sudoers"
+#wget -O - https://raw.githubusercontent.com/paulikappi/first-boot/master/first-boot.sh | sudo bash
 sudo echo "Defaults    timestamp_timeout=0" >> /etc/sudoers
-fi
 sudo apt update
 sudo apt upgrade -y
